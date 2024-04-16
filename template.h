238589adb72905e78ef346f747bd7e0a063ad5d4
selahaddinFurkanYildirim <110099952+selahaddinFurkanYildirim@users.noreply.github.com>
2024-04-16 21:56:13 +0300

    Create template.h

---
+++ template.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#define debug(a) _Generic((a), float:debugF,char:debugC,char*: debugS, int: debugD,int*:debugDX,float*:debugFX,char**:debugSX)(#a,a,'\0',sizeof(a))
+void debugD(char* n,int d,char end,int){
+	printf("%s:%d%c",n,d,end);
+}
+void debugF(char* n,float f,char end,int){
+	printf("%s:%f%c",n,f,end);
+}
+void debugC(char* n,char c,char end,int){
+	printf("%s:%c%c",n,c,end);
+}
+void debugS(char* n,char *s,char end,int){
+	printf("%s:%s%c",n,s,end);
+}
+void debugDX(char* n,int *s,char end,int size){
+	printf("%s:{",n);
+	for(int i=0;i<(int)(size/sizeof(int))-1;i++){
+		printf("%d,",s[i]);
+	}
+	printf("%d}",s[(int)(size/sizeof(int))-1]);
+	printf("%c",end);
+}
+void debugFX(char* n,float *s,char end,int size){
+	printf("%s:{",n);
+	for(int i=0;i<(int)(size/sizeof(float))-1;i++){
+		printf("%f,",s[i]);
+	}
+	printf("%f}",s[(int)(size/sizeof(float))-1]);
+	printf("%c",end);
+}
+void debugSX(char* n,char *s[],char end,int size){
+	printf("%s:{",n);
+	for(int i=0;i<(int)(size/sizeof(char*))-1;i++){
+		printf("\"%s\",",s[i]);
+	}
+	printf("\"%s\"}",s[(int)(size/sizeof(char*))-1]);
+	printf("%c",end);
+}
+
+
+// int main() {
+//     int intValue = 42;
+//     int intValues[]={45,15,5};
+//     char charValue = 'A';
+//     float floatValue = 3.14;
+//     float floatValues[] = {3.14,1.45,5.5};
+//     char* stringValue = "Hello, World!";
+//     char* stringValues[] = {"Hello, World!","mynameisblabla"};
+
+//     debug(intValue);
+//     debug(intValues);
+//     debug(charValue);
+//     debug(floatValue);
+//     debug(floatValues);
+//     debug(stringValue);
+//     debug(stringValues);
+//     return 0;
+// }
