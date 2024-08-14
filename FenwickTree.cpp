a281dd5cb0094e561bd36bc40f2d1debbcf08e02
selahaddinFurkanYildirim <110099952+selahaddinFurkanYildirim@users.noreply.github.com>
2024-08-14 12:45:00 +0300

    Update FenwickTree.cpp

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -6,39 +6,39 @@ typedef long long ll;
 vector<int> tree;
 
 void update(int x,int val){
-	x++;
-	int n=tree.size();
-	while(x<=n){
-		tree[x]+=val;
-		x+=x&(-x);
-	}
+    x++;
+    int n=tree.size();
+    while(x<=n){
+        tree[x]+=val;
+        x+=x&(-x);
+    }
 }
 
 void build(vector<int> &ar){
-	int n=ar.size();
+    int n=ar.size();
     tree.resize(n+1);
-	for(int i=0;i<n;i++){
-		update(i,ar[i]);
-	}
+    for(int i=0;i<n;i++){
+        update(i,ar[i]);
+    }
 }
 int query(int x){
-	int sum=0;
-	x++;
-	while(x){
-		sum+=tree[x];
-		x-=x&(-x);
-	}
-	return sum;
+    int sum=0;
+    x++;
+    while(x){
+        sum+=tree[x];
+        x-=x&(-x);
+    }
+    return sum;
 }
 int rangeQuery(int l,int r){
-	return query(r)-query(l-1);
+    return query(r)-query(l-1);
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout<<"Program derlenme tarihi: "<<__DATE__<<endl
-		<<"Program derlenme saati: "<<__TIME__<<endl;
+        <<"Program derlenme saati: "<<__TIME__<<endl;
     int n;
     cin>>n;
     vector<int> ar(n);
@@ -47,10 +47,10 @@ signed main() {
     int q;
     cin>>q;
     while(q--){
-		int x,y;
-		cin>>x>>y;
-		if(x>y) swap(x,y);
-		cout<<rangeQuery(x-1,y-1)<<endl;
-	}
+        int x,y;
+        cin>>x>>y;
+        if(x>y) swap(x,y);
+        cout<<rangeQuery(x-1,y-1)<<endl;
+    }
     return 0;
 }
