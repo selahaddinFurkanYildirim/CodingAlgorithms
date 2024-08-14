1e7f76c2e2ec3e5cba179e4a14cfb17f8010f6f0
selahaddinFurkanYildirim <110099952+selahaddinFurkanYildirim@users.noreply.github.com>
2024-08-14 12:39:32 +0300

    Update template.h

--- template.h
+++ template.h
@@ -9,40 +9,48 @@
 
 #ifdef __cplusplus
 #define debug(a) Debug(#a,a,'\n',sizeof(a))
-void Debug(char* n,int d,char E,int size){
-  printf("%s : %d%c",n,d,E);
+void Debug(string n,int d,char E,int size){
+  printf("%s : %lld%c",n.c_str(),d,E);
 }
-void Debug(char* n,float f,char E,int size){
-  printf("%s : %f%c",n,f,E);
+void Debug(string n,float f,char E,int size){
+  printf("%s : %f%c",n.c_str(),f,E);
 }
-void Debug(char* n,char c,char E,int size){
-  printf("%s : %c%c",n,c,E);
+void Debug(string n,char c,char E,int size){
+  printf("%s : %c%c",n.c_str(),c,E);
 }
-void Debug(char* n,char *s,char E,int size){
-  printf("%s : %s%c",n,s,E);
+void Debug(string n,char *s,char E,int size){
+  printf("%s : %s%c",n.c_str(),s,E);
 }
-void Debug(char* n,int *s,char E,int size){
-  printf("%s : {",n);
+void Debug(string n,int *s,char E,int size){
+  printf("%s : {",n.c_str());
   for(int i=0;i<(int)(size/sizeof(int))-1;i++){
-    printf("%d,",s[i]);
+    printf("%lld,",s[i]);
   }
-  printf("%d}",s[(int)(size/sizeof(int))-1]);
+  printf("%lld}",s[(int)(size/sizeof(int))-1]);
   printf("%c",E);
 }
-void Debug(char* n,float *s,char E,int size){
-  printf("%s : {",n);
+void Debug(string n,float *s,char E,int size){
+  printf("%s : {",n.c_str());
   for(int i=0;i<(int)(size/sizeof(float))-1;i++){
     printf("%f,",s[i]);
   }
   printf("%f}",s[(int)(size/sizeof(float))-1]);
   printf("%c",E);
 }
-void Debug(char* n,char *s[],char E,int size){
-  printf("%s : {",n);
+void Debug(string n,char *s[],char E,int size){
+  printf("%s : {",n.c_str());
   for(int i=0;i<(int)(size/sizeof(char*))-1;i++){
     printf("\"%s\",",s[i]);
   }
   printf("\"%s\"}",s[(int)(size/sizeof(char*))-1]);
+  printf("%c",E);
+}
+void Debug(string n,vector<int> s,char E,int size){
+  printf("%s : {",n.c_str());
+  for(int i=0;i<(int)(s.size())-1;i++){
+    printf("\"%lld\",",s[i]);
+  }
+  printf("\"%lld\"}",s[(int)(s.size())-1]);
   printf("%c",E);
 }
 #else
